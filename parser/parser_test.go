7c4023491762e281a84cc7094c34373218b2f5f7
Chris Prather <chris@prather.org>
2019-10-06 02:50:34 +0000

    WIP: parser

---
+++ parser/parser_test.go
@@ -0,0 +1,77 @@
+package parser
+
+import (
+	"testing"
+
+	"github.com/perigrin/simian/ast"
+	"github.com/perigrin/simian/lexer"
+)
+
+func TestLetStatments(t *testing.T) {
+	input := `
+	let x = 5;
+	let y = 10;
+	let foobar = 838383;
+	`
+	l := lexer.New(input)
+	p := New(l)
+
+	program := p.ParseProgram()
+	checkParseErrors(t, p)
+
+	if program == nil {
+		t.Fatalf("ParseProgram() returned nil")
+	}
+	if len(program.Statements) != 3 {
+		t.Fatalf("not enough statements: got %d expected %d", len(program.Statements), 3)
+	}
+
+	tests := []struct {
+		Identifier string
+	}{
+		{"x"},
+		{"y"},
+		{"foobar"},
+	}
+
+	for i, tt := range tests {
+		stmt := program.Statements[i]
+		if !testLetStatement(t, stmt, tt.Identifier) {
+			return
+		}
+	}
+}
+
+func testLetStatement(t *testing.T, s ast.Statement, name string) bool {
+	if s.TokenLiteral() != "let" {
+		t.Errorf("s.TokenLiteral not 'let': got %s", s.TokenLiteral())
+		return false
+	}
+	letStmt, ok := s.(*ast.LetStatement)
+	if !ok {
+		t.Errorf("s not *ast.LetStatement: got %T", s)
+		return false
+	}
+	if letStmt.Name.Value != name {
+		t.Errorf("letStmt.Name.Value != '%s': got %s", name, letStmt.Name.Value)
+		return false
+	}
+	if letStmt.Name.TokenLiteral() != name {
+		t.Errorf("letStmt.Name.TokenLiteral() != '%s': got %s", name, letStmt.Name.TokenLiteral())
+		return false
+	}
+	return true
+}
+
+func checkParseErrors(t *testing.T, p *Parser) {
+	errors := p.Errors()
+	if len(errors) == 0 {
+		return
+	}
+
+	t.Errorf("parser has %d errors", len(errors))
+	for _, msg := range errors {
+		t.Errorf("parse error: %s", msg)
+	}
+	t.FailNow()
+}
